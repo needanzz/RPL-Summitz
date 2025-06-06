e88df6a3f7e7df1dc7ef41825f02d21a356b6884
Muhammad Danil Aminuddin <145015784+needanzz@users.noreply.github.com>
2025-06-06 22:41:50 +0700

    Delete config directory

--- config/midtrans.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return[
-    'client_key' => env('MIDTRANS_CLIENT_KEY'),
-    'server_key' => env('MIDTRANS_SERVER_KEY'),
-    'is_productions' => env('MIDTRANS_IS_PRODUCTION', false),
-    'is_sanitized' => env('MIDTRANS_IS_SANITIZED', true),
-    'is_3ds' => env('MIDTRANS_IS_3DS', true),
-];
